b2c8b2882ce9bbc463744edf531c01b1afb01e47
liangxiaoming <398921432@qq.com>
2017-10-20 12:58:20 +0800

    right list 0.2

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,24 @@ setTimeout(function() {
 
 var app = {
     init: function(){
+        var self = this;
         $('.js-loading-wrap').addClass('hide');
         $('.js-game-wrap').removeClass('hide');
+        self.bindEven();
     },
     showGameWrap: function(){
-
+        var self = this;
+    },
+    bindEven: function(){
+        var self = this;
+        // 更多菜单弹出
+        $('.js-bt-more').on('click', function(){
+            $('.js-more-menu').removeClass('hide');
+        });
+        // 更多菜单收起
+        $('.js-more-menu .more-menu-mask').on('click', function(){
+            $('.js-more-menu').addClass('hide');
+        });
     }
 }
 
