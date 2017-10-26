ecc66bfacf85d1e7d266dd426cb53112348b1ea6
liangxiaoming <398921432@qq.com>
2017-10-26 13:11:33 +0800

    readme 上传2

--- src/js/tpl/pocketwrap.tpl.js
+++ src/js/tpl/pocketwrap.tpl.js
@@ -1,2 +1,2 @@
-/*TMODJS:{"version":12,"md5":"fda5ae354046bd92cb521c176b3bbd6a"}*/
-template("E:/wamp/www/Jeremy/panya/newweb/landlord/src/js/tpl/pocketwrap",function(a){"use strict";var b=this,c=(b.$helpers,b.$each),d=a.cardarr,e=(a.v,a.i,b.$escape),f=a.carddata,g="";return c(d,function(a){g+=' <img class="pok" src="',g+=e(f[a]),g+='" pknum="',g+=e(a),g+='" /> '}),new String(g)});
+/*TMODJS:{"version":13,"md5":"d8099fb409389ae74efd9ac6b0e8b6bf"}*/
+template("/Users/xiaominghari/Documents/wanrenqun/newsvn/web/landlord/landlord/src/js/tpl/pocketwrap",function(a){"use strict";var b=this,c=(b.$helpers,b.$each),d=a.cardarr,e=(a.v,a.i,b.$escape),f=a.carddata,g="";return c(d,function(a){g+=' <img class="pok" src="',g+=e(f[a]),g+='" pknum="',g+=e(a),g+='" /> '}),new String(g)});
