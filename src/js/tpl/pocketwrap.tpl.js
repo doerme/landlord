4b356c71773cf73e407dcd632fca338a12b687e9
liangxiaoming <398921432@qq.com>
2017-10-27 13:22:26 +0800

    talk ball

--- src/js/tpl/pocketwrap.tpl.js
+++ src/js/tpl/pocketwrap.tpl.js
@@ -1,2 +1,2 @@
-/*TMODJS:{"version":13,"md5":"d8099fb409389ae74efd9ac6b0e8b6bf"}*/
+/*TMODJS:{"version":14,"md5":"d8099fb409389ae74efd9ac6b0e8b6bf"}*/
 template("/Users/xiaominghari/Documents/wanrenqun/newsvn/web/landlord/landlord/src/js/tpl/pocketwrap",function(a){"use strict";var b=this,c=(b.$helpers,b.$each),d=a.cardarr,e=(a.v,a.i,b.$escape),f=a.carddata,g="";return c(d,function(a){g+=' <img class="pok" src="',g+=e(f[a]),g+='" pknum="',g+=e(a),g+='" /> '}),new String(g)});
